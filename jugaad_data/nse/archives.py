b351ebe63f6f02ed068d4cc6e3ce7f61d2c847ea
Gangadhar Shastri <gangadhar@aajkiawaz.in>
2020-08-16 14:22:24 +0000

    :zap: Added bhavcopy support

---
+++ jugaad_data/nse/archives.py
@@ -0,0 +1,106 @@
+from datetime import datetime, date
+import io
+import csv
+import zipfile
+import requests
+
+def unzip(function):
+    
+    def unzipper(*args, **kwargs):
+        r = function(*args, **kwargs)
+        fp = io.BytesIO(r)
+        with zipfile.ZipFile(file=fp) as zf:
+            fname = zf.namelist()[0]
+            with zf.open(fname) as fp_bh:
+                return fp_bh.read().decode('utf-8')
+    return unzipper
+
+
+class NSEArchives:
+    base_url = "https://archives.nseindia.com"
+    """Conventions
+           d - 1, 12 (without leading zero)
+          dd - 01, 21 (day of the month with leading zero)
+          mm - 01, 12 (month with leading zero)
+           m - 1, 12 (month without leading zero)
+         MMM - JAN, DEC
+          yy - 19, 20
+        yyyy - 2020, 2030
+    """
+    s = requests.Session()
+    h = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36",
+        "accept-encoding": "gzip, deflate, br",
+        "accept":
+"""text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9""",
+        
+}
+    s.headers.update(h)
+    _routes = {
+            "bhavcopy": "/content/historical/EQUITIES/{yyyy}/{MMM}/cm{dd}{MMM}{yyyy}bhav.csv.zip",
+            "bhavcopy_full": "/products/content/sec_bhavdata_full_{dd}{mm}{yyyy}.csv",
+            "bulk_deals": "/content/equities/bulk.csv"
+        }
+    
+    
+    def get(self, rout, **params):
+        url = self.base_url + self._routes[rout].format(**params)
+        self.r = self.s.get(url)
+        return self.r
+    
+    @unzip
+    def bhavcopy_raw(self, dt):
+        """Downloads raw bhavcopy text for a specific date"""
+        dd = dt.strftime('%d')
+        MMM = dt.strftime('%b').upper()
+        yyyy = dt.year
+        r = self.get("bhavcopy", yyyy=yyyy, MMM=MMM, dd=dd)
+        return r.content
+    
+    def bhavcopy_save(self, dt, fname):
+        """Downloads and saves raw bhavcopy csv file for a specific date"""
+        text = self.bhavcopy_raw(dt)
+        with open(fname, 'w') as fp:
+            fp.write(text)
+
+    def full_bhavcopy_raw(self, dt):
+        """Downloads full raw bhavcopy text for a specific date"""
+        
+        dd = dt.strftime('%d')
+        mm = dt.strftime('%m')
+        yyyy = dt.year
+        r = self.get("bhavcopy_full", yyyy=yyyy, mm=mm, dd=dd)
+        return r.text
+
+    def full_bhavcopy_save(self, dt, fname):
+        text = self.full_bhavcopy_raw(dt)
+        with open(fname, 'w') as fp:
+            fp.write(text)
+
+    def bulk_deals_raw(self):
+        r = self.get("bulk_deals")
+        return r.text
+    
+    def bulk_deals_save(self, fname):
+        text = self.bulk_deals_raw()
+        with open(fname, 'w') as fp:
+            fp.write(text)
+
+a = NSEArchives()
+bhavcopy_raw = a.bhavcopy_raw
+bhavcopy_save = a.bhavcopy_save
+full_bhavcopy_raw = a.full_bhavcopy_raw
+full_bhavcopy_save = a.full_bhavcopy_save
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ tests/test_daily.py
@@ -0,0 +1,17 @@
+from datetime import date
+from jugaad_data.nse import bhavcopy_raw, full_bhavcopy_raw
+
+
+def test_bhavcopy():
+    r = bhavcopy_raw(date(2020,1,1))
+    header = "SYMBOL,SERIES,OPEN,HIGH,LOW,CLOSE,LAST,PREVCLOSE,TOTTRDQTY,TOTTRDVAL,TIMESTAMP,TOTALTRADES,ISIN"
+    assert "RELIANCE" in r
+    assert header in r
+
+def test_full_bhavcopy():
+    r = full_bhavcopy_raw(date(2020,1,1))
+    header = "SYMBOL, SERIES, DATE1, PREV_CLOSE, OPEN_PRICE, HIGH_PRICE, LOW_PRICE, LAST_PRICE, CLOSE_PRICE, AVG_PRICE, TTL_TRD_QNTY, TURNOVER_LACS, NO_OF_TRADES, DELIV_QTY, DELIV_PER"
+    assert "SBIN" in r
+    print(r[0:200])
+    assert header in r
+    
