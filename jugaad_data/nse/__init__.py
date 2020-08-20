e5e7f943f515a8b24a8b5c109b4ea439f63df92d
Horse with no name <nomail@nodomain.com>
2020-08-20 14:07:11 +0000

    :rocket: added derivative bhavcopy

--- jugaad_data/nse/__init__.py
+++ jugaad_data/nse/__init__.py
@@ -14,7 +14,7 @@ except:
     pd = None
 
 from jugaad_data import util as ut
-from .archives import bhavcopy_raw, bhavcopy_save, full_bhavcopy_raw, full_bhavcopy_save
+from .archives import bhavcopy_raw, bhavcopy_save, full_bhavcopy_raw, full_bhavcopy_save, bhavcopy_fo_raw, bhavcopy_fo_save
 
 APP_NAME = "nsehistory"
 class NSEHistory:
--- tests/test_bhav.py
+++ tests/test_bhav.py
@@ -1,5 +1,5 @@
 from datetime import date
-from jugaad_data.nse import bhavcopy_raw, full_bhavcopy_raw
+from jugaad_data.nse import bhavcopy_raw, full_bhavcopy_raw, bhavcopy_fo_raw
 
 
 def test_bhavcopy():
@@ -12,8 +12,15 @@ def test_full_bhavcopy():
     r = full_bhavcopy_raw(date(2020,1,1))
     header = "SYMBOL, SERIES, DATE1, PREV_CLOSE, OPEN_PRICE, HIGH_PRICE, LOW_PRICE, LAST_PRICE, CLOSE_PRICE, AVG_PRICE, TTL_TRD_QNTY, TURNOVER_LACS, NO_OF_TRADES, DELIV_QTY, DELIV_PER"
     assert "SBIN" in r
-    print(r[0:200])
     assert header in r
+
+def test_bhavcopy_fo():
+    r = bhavcopy_fo_raw(date(2020,1,1))
+    header = "INSTRUMENT,SYMBOL,EXPIRY_DT,STRIKE_PR,OPTION_TYP,OPEN,HIGH,LOW,CLOSE,SETTLE_PR,CONTRACTS,VAL_INLAKH,OPEN_INT,CHG_IN_O"
+    assert "SBIN" in r
+    assert header in r
+
+
 """
 def test_bhavcopy_on_holiday():
     r = bhavcopy_raw(date(2020,1,5))
