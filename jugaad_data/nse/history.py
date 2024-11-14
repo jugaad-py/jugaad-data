efef07de983b2344886d32e111cdf2da59d6aedf
k-prabin <155169282+k-prabin@users.noreply.github.com>
2024-11-14 22:13:02 +0530

    Update history.py

--- jugaad_data/nse/history.py
+++ jugaad_data/nse/history.py
@@ -38,7 +38,7 @@ class NSEHistory:
             "sec-fetch-site": "same-origin",
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36",
             "Accept": "*/*",
-            "Accept-Encoding": "gzip, deflate, br",
+            "Accept-Encoding": "gzip, deflate",
             "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
             "Cache-Control": "no-cache",
             "Connection": "keep-alive",
