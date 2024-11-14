9119be63dc64738d08104484d98d6701fb037ad3
k-prabin <155169282+k-prabin@users.noreply.github.com>
2024-11-14 22:13:43 +0530

    Update archives.py

--- jugaad_data/nse/archives.py
+++ jugaad_data/nse/archives.py
@@ -147,7 +147,7 @@ class NSEIndicesArchives(NSEArchives):
         "X-Requested-With": "XMLHttpRequest",
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36",
        "Accept": "*/*",
-       "Accept-Encoding": "gzip, deflate, br",
+       "Accept-Encoding": "gzip, deflate",
        "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
        "Cache-Control": "no-cache",
        "Connection": "keep-alive",
